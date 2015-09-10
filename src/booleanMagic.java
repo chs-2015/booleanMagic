1c9f3357cc1f3a1e11698eac0cbfc0018d6450a5
Brandon Berney <brandonberney@protonmail.com>
2015-09-10 13:03:01 -0400

    magic!

---
+++ src/booleanMagic.java
@@ -0,0 +1,17 @@
+/**
+ * @(#)booleanMagic.java
+ *
+ * booleanMagic application
+ *
+ * @author 
+ * @version 1.00 2015/9/10
+ */
+ 
+public class booleanMagic {
+    
+    public static void main(String[] args) {
+    	
+    	// TODO, add your application code
+    	System.out.println("Hello World!");
+    }
+}
